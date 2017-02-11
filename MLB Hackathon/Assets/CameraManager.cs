a3435b348c0fa9ef4d2456cd0b093ccc38545177
jchin <justinchin.bcchs@gmail.com>
2017-02-11 13:25:23 -0500

    Added camera manager to switch and enable different main cameras

---
+++ MLB Hackathon/Assets/CameraManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public List<Camera> cameraList = new List<Camera>();
+    public Dictionary<string, Camera> cameraDictionary = new Dictionary<string, Camera>();
+    public Camera ActiveCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        //Set up playerDictionary
+        foreach (Camera camera in cameraList)
+        {
+            cameraDictionary.Add(camera.name, camera);
+        }
+        ActiveCamera = cameraDictionary["Main Camera"];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        EventManager.Instance.AddListener<CameraEvent>(OnCameraEvent);
+    }
+
+    void OnDisable()
+    {
+        EventManager.Instance.RemoveListener<CameraEvent>(OnCameraEvent);
+    }
+
+    void OnCameraEvent(CameraEvent e)
+    {
+        Camera camera = cameraDictionary[e.CameraName];
+        ActiveCamera.enabled = false;
+        ActiveCamera = camera;
+        ActiveCamera.enabled = true;
+
+
+        //Debug.Log("<color=red>Audio Event happened</color>" + e.Name);
+    }
+}
---
+++ MLB Hackathon/Assets/Scripts/CameraEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEvent : GameEvent
+{
+    public string CameraName;
+
+    public CameraEvent(float startEventTime, string cameraName)
+    {
+        eventTime = startEventTime;
+        CameraName = cameraName;
+    }
+}
