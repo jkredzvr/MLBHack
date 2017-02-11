f7bfcd18cedf52537a86b8930e8cde985705deb9
jchin <justinchin.bcchs@gmail.com>
2017-02-11 10:39:53 -0500

    Added string name and startEventTime to the GameEvent class For EventManager, it checks current time and whether it is time to execute event create fake sount event listener

---
+++ MLB Hackathon/Assets/Scripts/AudioEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioEvent : GameEvent {
+
+    public string Name;
+
+    public AudioEvent(float startEventTime, string name)
+    {
+        eventTime = startEventTime;
+        Name = name;
+    }
+}
---
+++ MLB Hackathon/Assets/Scripts/FakeSoundEventListener.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FakeSoundEventListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        EventManager.Instance.QueueEvent(new AudioEvent(1, "yea"));
+        EventManager.Instance.QueueEvent(new AudioEvent(5, "hi asfddsaf"));
+        EventManager.Instance.QueueEvent(new AudioEvent(10,"heyo"));
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnEnable()
+    {
+        EventManager.Instance.AddListener<AudioEvent>(OnAudioEvent);
+    }
+    void OnDisable()
+    {
+        EventManager.Instance.RemoveListener<AudioEvent>(OnAudioEvent);
+    }
+
+    void OnAudioEvent(AudioEvent e)
+    {
+        Debug.Log("<color=red>Audio Event happened</color>" + e.Name);
+    }
+}
