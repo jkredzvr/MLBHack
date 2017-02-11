1a8deb0d6239ba99c1c8b2a1c3971c03b5b6447e
jchin <justinchin.bcchs@gmail.com>
2017-02-11 18:30:51 -0500

    Player behvaiour allows for translation of models

--- MLB Hackathon/Assets/PlayerBehaviour.cs
+++ MLB Hackathon/Assets/PlayerBehaviour.cs
@@ -3,19 +3,47 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public float speed = .1f;
+
+    private Vector3 start;
+    private Vector3 des;
+    private float fraction = 0;
+    bool isMoving = false;
+
+    // Use this for initialization
+    void Start () {
+        start = transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (isMoving)
+        {
+            Moving();
+        }
 	}
 
-    public void Move(Vector3 startPos, Vector3 endPos)
+    public void SetMove(Vector3 startPos, Vector3 endPos)
     {
         Debug.Log("StartPos:" + startPos);
         Debug.Log("EndPos:" + endPos);
+        start = startPos;
+        des = endPos;
+        isMoving = true;
+        
+    }
+
+    public void Moving()
+    {
+        if (fraction < 1)
+        {
+            fraction += Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(start, des, fraction);
+        }
+        else
+        {
+            fraction = 0;
+            isMoving = false;
+        }
     }
 }
