217383a46ee306e6a2f46cfaab199458fe02c236
jchin <justinchin.bcchs@gmail.com>
2017-02-11 23:56:48 -0500

    Camera stablizer script for controlling camera

---
+++ MLB Hackathon/Assets/CameraStabilizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraStabilizer : MonoBehaviour {
+    public Transform headPos;
+    public Vector3 newHeadPos;
+    public Vector3 curHeadPos;
+    //public Vector3 curRot; public Vector3 deltaRot;
+
+    // Use this for initialization
+    void Start () {
+        //curRot = transform.localEulerAngles;
+        curHeadPos = transform.position;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        newHeadPos = headPos.position;
+        this.transform.position = newHeadPos;
+        
+        
+        //this.transform.position = new Vector3 (headPos.position.x, curHeadPos.y, headPos.position.z);
+
+        //deltaRot = curRot - transform.localEulerAngles;
+        //transform.localEulerAngles = curRot+deltaRot;
+
+    }
+}
