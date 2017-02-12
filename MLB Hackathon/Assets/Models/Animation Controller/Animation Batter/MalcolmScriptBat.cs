17b12a8a78130da2edad9e89c9bba0a7aa7a3612
jchin <justinchin.bcchs@gmail.com>
2017-02-11 23:48:32 -0500

    2 Models with animations

---
+++ MLB Hackathon/Assets/Models/Animation Controller/Animation Batter/MalcolmScriptBat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    //Animator anim;
+    Animator myAnimator;
+    // Use this for initialization
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        myAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float time =  
+        //float move = Input.time
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+    }
+}
